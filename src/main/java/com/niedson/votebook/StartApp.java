401f5c5c4541f638bbc718cf9735457e47780049
niedson <niedson.araujo.c@gmail.com>
2015-03-31 12:08:55 -0300

    heroku

--- src/main/java/com/niedson/votebook/StartApp.java
+++ src/main/java/com/niedson/votebook/StartApp.java
@@ -1,26 +1,26 @@
 package com.niedson.votebook;
 
 
-import com.niedson.votebook.model.spring.SpringApplicationContext;
-import com.niedson.votebook.persistence.entity.Book;
+//import com.niedson.votebook.model.spring.SpringApplicationContext;
+//import com.niedson.votebook.persistence.entity.Book;
 
 	public class StartApp {
-	
-	public static void main(String[] args) throws Exception {
-		Book book = new Book();
-		book.setName("Senhor dos Aneis");
-		
-//		BookDAO bookDAO = SpringApplicationContext.DAO.getBookDAO();
-//		Long id = bookDAO.save(book);
-//		System.out.println("Id: " + id);
+//	
+//	public static void main(String[] args) throws Exception {
+//		Book book = new Book();
+//		book.setName("Senhor dos Aneis");
 //		
-//		List<Book> listAll = bookDAO.listAll();
-//		System.out.println("Count: " + listAll.size());
-//		for (Book book2: listAll) {
-//			System.out.println("Book name:" + book2.getName());
-//		}
-         
-        SpringApplicationContext.closeApplicationContext();
-	}
-	
+////		BookDAO bookDAO = SpringApplicationContext.DAO.getBookDAO();
+////		Long id = bookDAO.save(book);
+////		System.out.println("Id: " + id);
+////		
+////		List<Book> listAll = bookDAO.listAll();
+////		System.out.println("Count: " + listAll.size());
+////		for (Book book2: listAll) {
+////			System.out.println("Book name:" + book2.getName());
+////		}
+//         
+//        SpringApplicationContext.closeApplicationContext();
+//	}
+//	
 }
